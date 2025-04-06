728a3a1937c6b5efd9044e439d40965aef674c94
Robbert Wijtman <robbert@xinxmedia.com>
2025-04-06 08:35:33 -0700

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { Canvas, Rect } from '@bucky24/react-canvas';
 import { Polygon } from '@bucky24/toolbox';
 
@@ -10,11 +10,18 @@ import config from './config';
 export default function App() {
 	const [size, setSize] = useState({ width: 0, height: 0 });
 	const { items, takeAction, processItems } = useContext(LayoutContext);
+	const canvasHolderRef = useRef();
 
 	const resize = () => {
+		if (!canvasHolderRef.current) {
+			return;
+		}
+
+		const rect = canvasHolderRef.current.getBoundingClientRect();
+		
 		setSize({
-			width: window.innerWidth,
-			height: innerHeight,
+			width: rect.width,
+			height: rect.height,
 		});
 	}
 
@@ -27,52 +34,75 @@ export default function App() {
 		}
 	}, []);
 
-	return (<div className={styles.appRoot}>
-		<Canvas
-			width={size.width}
-			height={size.height}
-			onMouseUp={(({ x, y, button }) => {
-				// try to find an element we are clicking on
-				let clickedElement = null;
+	useEffect(() => {
+		resize();
+	}, [canvasHolderRef.current]);
 
-				for (const item of items) {
-					const itemConfig = config.components[item.type];
-
-					if (!itemConfig) {
-						continue;
-					}
-
-					const width = itemConfig.width;
-					const height = itemConfig.height;
-					const ix = item.x;
-					const iy = item.y;
-
-					const clicked = Polygon.pointInsidePolygon(
-						{x,y},
-						[
-							{x:ix, y:iy},
-							{x:ix+width, y:iy},
-							{x:ix+width, y:iy+height},
-							{x:ix, y:iy+height},
-						],
-					);
-
-					if (clicked) {
-						clickedElement = item;
-						break;
-					}
-				}
-
-				if (clickedElement) {
-					takeAction(clickedElement, "click");
-				}
-			})}
-		>
-			<Rect x={0} y={0} x2={size.width} y2={size.height} color="#fff" fill={true} />
-			<CircuitBoard />
-		</Canvas>
-		<div style={{ position: 'absolute', top: 0, left: 0 }}>
+	return (<div className={styles.appRoot} style={{
+		display: 'flex',
+		flexDirection: 'column',
+	}}>
+		<div>
 			<button onClick={processItems}>Step</button>
+		</div>
+		<div style={{
+			flexGrow: 1,
+			display: 'flex',
+			flexDirection: 'row',
+		}}>
+			<div ref={canvasHolderRef} style={{
+				flexGrow: 1,
+			}}>
+				<Canvas
+					width={size.width}
+					height={size.height}
+					onMouseUp={(({ x, y, button }) => {
+						// try to find an element we are clicking on
+						let clickedElement = null;
+
+						for (const item of items) {
+							const itemConfig = config.components[item.type];
+
+							if (!itemConfig) {
+								continue;
+							}
+
+							const width = itemConfig.width;
+							const height = itemConfig.height;
+							const ix = item.x;
+							const iy = item.y;
+
+							const clicked = Polygon.pointInsidePolygon(
+								{x,y},
+								[
+									{x:ix, y:iy},
+									{x:ix+width, y:iy},
+									{x:ix+width, y:iy+height},
+									{x:ix, y:iy+height},
+								],
+							);
+
+							if (clicked) {
+								clickedElement = item;
+								break;
+							}
+						}
+
+						if (clickedElement) {
+							takeAction(clickedElement, "click");
+						}
+					})}
+				>
+					<Rect x={0} y={0} x2={size.width} y2={size.height} color="#fff" fill={true} />
+					<CircuitBoard />
+				</Canvas>
+			</div>
+			<div style={{
+				flexShrink: 0,
+				flexBasis: 200,
+			}}>
+				sidebar
+			</div>
 		</div>
 	</div>);
 }
